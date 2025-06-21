aacf633f5e716f1bc50dc097dcf0437d360a4a2f
Midasta <60594967+Midasta@users.noreply.github.com>
2025-06-21 15:28:38 +0400

    Update sw.js

--- sw.js
+++ sw.js
@@ -8,14 +8,31 @@ const filesToCache = [
   "./favicon.ico"
 ];
 
+// Установка
 self.addEventListener("install", (e) => {
   e.waitUntil(
-    caches.open(cacheName).then((cache) => {
-      return cache.addAll(filesToCache);
+    caches.open(cacheName)
+      .then((cache) => cache.addAll(filesToCache))
+      .catch((err) => console.error("Cache error:", err))
+  );
+});
+
+// Обновление и замена старого кэша
+self.addEventListener("activate", (e) => {
+  e.waitUntil(
+    caches.keys().then((keyList) => {
+      return Promise.all(
+        keyList.map((key) => {
+          if (key !== cacheName) {
+            return caches.delete(key);
+          }
+        })
+      );
     })
   );
 });
 
+// Перехват запросов
 self.addEventListener("fetch", (e) => {
   e.respondWith(
     caches.match(e.request).then((response) => {
